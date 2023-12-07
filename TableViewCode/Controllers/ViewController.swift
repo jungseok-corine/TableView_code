5479369b77edcc318ed103aabb2af5796d9defa9
jungseok-corine <nonehour@naver.com>
2023-12-07 12:20:23 +0900

    시작

---
+++ TableViewCode/Controllers/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TableViewCode
+//
+//  Created by 오정석 on 7/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    var movieArray: [Movie] = []
+    
+    var movieDataManger = DataManager()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+    func setupNaviBar() {
+        title = "영화 목록"
+        
+        
+    }
+    
+    
+    
+    
+
+}
+
---
+++ TableViewCode/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  TableViewCode
+//
+//  Created by 오정석 on 7/12/2023.
+//
+
+import Foundation
+
+
+struct Movie {
+    let movieImage: UIImage?
+    var movieName: String
+    var description: String
+}
